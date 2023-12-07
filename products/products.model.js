55f7a0adbc2e6fac3a73d0d6e9f75e309d839611
S-O <s@example.com>
2023-12-07 13:06:04 +0530

    graphql-tools like makeExecutableSchema and resolvers added"

---
+++ products/products.model.js
@@ -0,0 +1,4 @@
+module.exports = [
+  { id: "redshoe", description: "Red Shoe", price: 41.33 },
+  { id: "bluejeans", description: "Blue Jeans", price: 55.67 },
+];
--- server.js
+++ server.js
@@ -1,59 +1,32 @@
 const express = require("express");
-const { buildSchema } = require("graphql");
+const path = require("path");
 const { graphqlHTTP } = require("express-graphql");
 
-const schema = buildSchema(`
-  type Query {
-    products: [Product]
-    orders: [Order]
-  }
+const { makeExecutableSchema } = require("@graphql-tools/schema");
+const { loadFiles, loadFilesSync } = require("@graphql-tools/load-files");
 
-  type Product {
-    id: ID!
-    description: String!
-    price: Float!
-    reviews: [Review]
-  }
+const typesArray = loadFilesSync(path.join(__dirname, "**/*.graphql"));
 
-  type Review {
-    rating: Int!
-    comment: String
-  }
-
-  type Order {
-    date: String!
-    subtotal: Float!
-    items: [OrderItem]
-
-  }
-
-  type OrderItem {
-    product: Product!
-    quantity: Int!
-  }
-`);
+const schema = makeExecutableSchema({
+  typeDefs: typesArray,
+  resolvers: {
+    Query: {
+      products: async (parent) => {
+        console.log("Getting the Products...");
+        const products = await Promise.resolve(parent.products);
+        return products;
+      },
+      orders: (parent) => {
+        console.log("Getting the Orders...");
+        return parent.orders;
+      },
+    },
+  },
+});
 
 const root = {
-  products: [
-    { id: "redshoe", description: "Red Shoe", price: 41.33 },
-    { id: "bluejeans", description: "Blue Jeans", price: 55.67 },
-  ],
-  orders: [
-    {
-      date: "2005-05-05",
-      subtotal: 90.22,
-      items: [
-        {
-          product: {
-            id: "redshoe",
-            description: "old red shoe",
-            price: 45.11,
-          },
-          quantity: 2,
-        },
-      ],
-    },
-  ],
+  products: require("./products/products.model"),
+  orders: require("./orders/orders.model"),
 };
 
 const app = express();
